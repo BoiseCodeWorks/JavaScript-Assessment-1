27442d0df6b24a2fe9ded1e2a2cfbb3abd646e29
Jake Overall <joverall22@gmail.com>
2015-06-24 19:35:51 -0600

    Update strings.js

--- strings.js
+++ strings.js
@@ -6,3 +6,7 @@ var sentence = 'When i went to the mall i bought a pair of shoes.';
 //2. The history of JavaScript is interesting but how many times does the letter a appear? Find a way to count both upper and lower case A's
 
 var jsHistory = 'JavaScript, not to be confused with Java, was created in 10 days in May 1995 by Brendan Eich, then working at Netscape and now of Mozilla. JavaScript was not always known as JavaScript: the original name was Mocha, a name chosen by Marc Andreessen, founder of Netscape. In September of 1995 the name was changed to LiveScript, then in December of the same year, upon receiving a trademark license from Sun, the name JavaScript was adopted. This was somewhat of a marketing move at the time, with Java being very popular around then.';
+
+//3. Given the following sentence uppercase the work scream each time it appears in the sentence
+
+var text = 'I scream, you scream, we all scream for icecream';
